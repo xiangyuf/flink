0a234e49bc027e8b4c663ef68e798c67f8f552d7
张云帆 <yunfanfighting@foxmail.com>
2023-06-15 04:42:05 +0000

    [#13213276] Flink-1.17 support byted hive catalog
    * [#13213276] Flink-1.17 support byted hive catalog
    * Resolve comment
    
    https://code.byted.org/inf/flink/merge_requests/5229

--- flink-connectors/flink-connector-hive/src/main/java/org/apache/flink/table/catalog/hive/client/HiveShimLoader.java
+++ flink-connectors/flink-connector-hive/src/main/java/org/apache/flink/table/catalog/hive/client/HiveShimLoader.java
@@ -30,6 +30,7 @@ import java.util.concurrent.ConcurrentHashMap;
 /** A loader to load HiveShim. */
 public class HiveShimLoader {
 
+    public static final String HIVE_VERSION_V1_2_2 = "1.2.2";
     public static final String HIVE_VERSION_V2_3_0 = "2.3.0";
     public static final String HIVE_VERSION_V2_3_1 = "2.3.1";
     public static final String HIVE_VERSION_V2_3_2 = "2.3.2";
@@ -55,6 +56,9 @@ public class HiveShimLoader {
         return hiveShims.computeIfAbsent(
                 version,
                 (v) -> {
+                    if (v.startsWith(HIVE_VERSION_V1_2_2)) {
+                        return new HiveShimV122();
+                    }
                     if (v.startsWith(HIVE_VERSION_V2_3_0)) {
                         return new HiveShimV230();
                     }
